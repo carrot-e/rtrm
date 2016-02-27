a488cf52b6c430a29f4b0cdbcfe7d3b9fc65d813
elena <elenalistratova11@gmail.com>
2016-02-27 22:48:08 +0200

    code for google search console & for GA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="google-site-verification" content="RdPr_Xe897UsknsB_Fj03fqZxBAuagidQBeLaLNz2CA" />
     <link rel="icon" href="/img/favicon.ico">
     <title>Routerium</title>
     <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,200' rel='stylesheet' type='text/css'>
@@ -58,5 +59,15 @@
 <script src="/bower_components/tinymce-dist/tinymce.min.js"></script>
 <script src="/bower_components/angular-ui-tinymce/src/tinymce.js"></script>
 @endif
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-74405426-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
